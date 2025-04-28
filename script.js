ac4cbb1ed2ae35e1d0b3cb4141ba92760e90e31f
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-04-28 11:53:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image from ${url}`));
+  });
+}
+function downloadImages(urls) {
+  const loadingDiv = document.getElementById("loading");
+  const errorDiv = document.getElementById("error");
+  const outputDiv = document.getElementById("output");
+  errorDiv.innerText = "";
+  outputDiv.innerHTML = "";
+  loadingDiv.style.display = "block";
+
+  const imagePromises = urls.map(downloadImage);
+
+  Promise.all(imagePromises)
+    .then(images => {
+      images.forEach(img => outputDiv.appendChild(img));
+    })
+    .catch(err => {
+      errorDiv.innerText = err.message;
+    })
+    .finally(() => {
+      loadingDiv.style.display = "none";
+    });
+}
+downloadImages(imageUrls);
